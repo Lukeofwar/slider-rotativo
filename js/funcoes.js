f8539f3636c1bfc6a375c50fdd4b0982ab73d820
Lukeofwar <matheuslucaskid@gmail.com>
2021-04-13 19:16:36 -0300

    terminei o siler com pucas dificuldades

---
+++ js/funcoes.js
@@ -0,0 +1,23 @@
+/** 
+* Feito por Lucas developer
+* Simples slide responsivo
+*/ 
+
+var intervalo = 2000;
+
+function slide1() {
+    document.getElementById("slide").src = "_imagens/img-2.jpg";
+ setTimeout("slide2()",intervalo);
+}
+
+function slide2() {
+    document.getElementById("slide").src = "_imagens/img-3.jpg";
+    setTimeout("slide3()",intervalo);
+
+}
+function slide3() {
+    document.getElementById("slide").src = "_imagens/img-5.jpg";
+    setTimeout("slide1()",intervalo);
+
+}
+
